93eceec108cb56b76a8155916a78bdd62258ffb0
Mark Cowley <m.cowley0@gmail.com>
2011-12-01 03:02:27 +0000

    v1

---
+++ R/grow.bed.R
@@ -0,0 +1,30 @@
+#' grow the intervals in a bed file
+#'
+#' The galaxy \code{flank} tool returns only the flanks, not the original
+#' regions. \code{grow} aims to provide the functionality of \code{flank},\
+#' but also return the original regions. ie, the seed regions will grow.
+#' Since the regions grow, an \code{offset} is not supported.
+#' 
+#' @section TODO:
+#' Add a check to make sure the regions don't extend beyond the end of a
+#' chromosome.
+#' 
+#' @param x a \code{data.frame} from \code{\link{import.ucsc.blat}}
+#' @param upstream numeric(1) indicating the nuber of bases to grow
+#'  in the upstream direction. default=0
+#' @param downstream numeric(1) indicating the nuber of bases to grow
+#'  in the downstream direction. default=0
+#' @return
+#' @author Mark Cowley, 2011-11-29
+#' @export
+grow.bed <- function(x, upstream=0, downstream=0) {
+	if("strand" %in% colnames(x)) strand <- x$strand
+	else strand <- rep("+", nrow(x))
+	
+	x$chromStart <- as.integer(x$chromStart - ifelse(strand =="+", rep(upstream,nrow(x)), rep(downstream,nrow(x))))
+	x$chromEnd <- as.integer(x$chromEnd + ifelse(strand =="+", rep(downstream,nrow(x)), rep(upstream,nrow(x))))
+	x$chromStart <- as.integer(pmax(0, x$chromStart))
+	# @TODO make sure that the chromosomes haven't become too long!
+	
+	x
+}
---
+++ R/midpoint.bed.R
@@ -0,0 +1,19 @@
+#' Determine the midpoint of an interval
+#'
+#' @param x a \code{data.frame} representation of a BED file
+#' @return a \code{data.frame} like \code{x}, with chromStart and chromEnd
+#'  updated to reflect the midpoint.
+#' 
+#' @author Mark Cowley, 2011-11-29
+#' @export
+#' @examples
+#' bed <- data.frame(chrom="chr1", chromStart=c(100,200,300), chromEnd=c(110, 250, 500))
+#' midpoint.bed(bed)
+midpoint.bed <- function(x) {
+	a <- x$chromStart + (x$chromEnd-x$chromStart)/2
+	a <- floor(a)
+	x$chromStart <- as.integer(a)
+	x$chromEnd <- as.integer(a)
+	
+	x
+}
