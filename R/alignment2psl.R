9b1f0d530a03aee380eb6a8f7868159df52ae976
Mark Cowley <m.cowley0@gmail.com>
2013-05-30 09:04:10 +0000

    roxygenized & refactored p->paste0

--- R/alignment2psl.R
+++ R/alignment2psl.R
@@ -1,19 +1,21 @@
-# Take an alignment of targets to chromosome(s) where the minimum
-# requirements are the query name, target name (ie chromosome),
-# start, stop and strand of the alignment.
-#
-# Parameters:
-# 	query: a character vector of query names
-# 	target: a vector of chromosome numbers; either chr1, chrX or 1, "X" etc...
-# 	start/stop: numerical vectors of start/stop of the alignment
-# 	strand: "+" or "-" for each alignment
-#
-# Value:
-#  a psl data.frame where it is assumed that the alignment is a perfect one between
-#  the query and the target, with only one alignment block.
-#
-# Mark Cowley, 24 August 2006
-#
+#' Take an alignment of targets to chromosome(s) where the minimum
+#' requirements are the query name, target name (ie chromosome),
+#' start, stop and strand of the alignment.
+#'
+#' @param query a character vector of query names
+#' @param target a vector of chromosome numbers; either chr1, chrX or 1, "X" etc...
+#' @param start numerical vectors of start of the alignment
+#' @param stop numerical vectors of stop of the alignment
+#' @param strand "+" or "-" for each alignment
+#' @param score alignment score. default=\code{NULL}
+#' @param x the alignment object
+#' 
+#' @return 
+#'  a psl data.frame where it is assumed that the alignment is a perfect one between
+#'  the query and the target, with only one alignment block.
+#'
+#' @author Mark Cowley, 24 August 2006
+#' @export
 alignment2psl <- function(query=x[,"query"], target=x[,"chr"], start=x[,"start"], stop=x[,"stop"], strand=x[,"strand"], score=NULL, x) {
 	match <- stop - start
 
@@ -40,7 +42,7 @@ alignment2psl <- function(query=x[,"query"], target=x[,"chr"], start=x[,"start"]
     #
 	if( any(target %in% c(1:30,"X","Y","M")) ) {
         idx <- which( target %in% c(1:30,"X","Y","M") )
-        target[idx] <- p("chr", target[idx])
+        target[idx] <- paste0("chr", target[idx])
 	}
 
 	psl <- new.psl(length(query), !is.null(score), match)
@@ -49,7 +51,7 @@ alignment2psl <- function(query=x[,"query"], target=x[,"chr"], start=x[,"start"]
 	psl$"T start" <- start
 	psl$"T end" <- stop
 	psl$"strand" <- strand
-	psl$"tStarts" <- p(start, ",")
+	psl$"tStarts" <- paste0(start, ",")
 	psl$"score" <- score
 
 	return( psl )
